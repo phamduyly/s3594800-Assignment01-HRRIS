0bb288a8a58851ae2a5e868ce0e1b8cce712a3a1
Unknown <s3594800@rmit.edu.vn>
2017-04-10 12:57:52 +0700

    Commit back to original repo

---
+++ HRRIS/Login.vb
@@ -0,0 +1,21 @@
+﻿Option Strict On
+Option Explicit On
+
+'Name: Login form 
+'date: 6/4/2017
+'Author: Ly Pham Duy
+'Purpose: add login to the program
+
+Public Class Login
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If (txtUserName.Text = "Admin") And (txtPass.Text = "12345") Then
+            Dim nav As New Navigation
+            nav.Show()
+            Me.Hide()
+        Else
+            MsgBox("Your Username/PassWord is not right")
+        End If
+    End Sub
+
+End Class
