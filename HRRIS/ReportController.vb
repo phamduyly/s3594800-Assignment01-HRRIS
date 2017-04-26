64ea7c72c140e90aae358522edd5742de94b4a17
phamduyly <s3594800@rmit.edu.vn>
2017-04-26 18:37:25 +0700

    BreakReport2 commit - need fixing

--- HRRIS/ReportController.vb
+++ HRRIS/ReportController.vb
@@ -347,6 +347,54 @@ Public Class ReportController
 
         Return lsData
     End Function
+    
+    'Break reprot 2 
+      Public Function BreakReport1(ByVal iMonths As Integer, ByVal iYears As Integer) As List(Of Hashtable)
+
+        Dim oConnection As OleDbConnection = New OleDbConnection(CONNECTION_STRING)
+        Dim lsData As New List(Of Hashtable)
+
+        Try
+            oConnection.Open()
+            Debug.Print("Connection String: " & oConnection.ConnectionString)
+
+
+            Dim oCommand As OleDbCommand = New OleDbCommand
+            oCommand.Connection = oConnection
+            'date from invoice table 
+            '" AND DatePart(""m"", date) = " & iMonths & 
+            oCommand.CommandText = "SELECT * FROM invoice WHERE DatePart (""yyyy"", date) = " & iYears & ";"
+
+            oCommand.Prepare()
+            Dim oDataReader = oCommand.ExecuteReader()
+
+            Dim htTempData As Hashtable
+            Do While oDataReader.Read() = True
+                htTempData = New Hashtable
+                htTempData("booking_id") = CInt(oDataReader("booking_id"))
+                htTempData("booking_date") = CDate(oDataReader("booking_date"))
+                htTempData("room_id") = CInt(oDataReader("room_id"))
+                htTempData("customer_id") = CInt(oDataReader("customer_id"))
+                htTempData("num_days") = CInt(oDataReader("num_days"))
+                htTempData("num_guests") = CInt(oDataReader("num_guests"))
+                htTempData("checkin_date") = CDate(oDataReader("checkin_date"))
+                htTempData("total_price") = CInt(oDataReader("total_price"))
+                htTempData("comments") = CStr(oDataReader("comments"))
+                lsData.Add(htTempData)
+            Loop
+
+            Debug.Print("the record was found.")
+
+        Catch ex As Exception
+            Debug.Print("ERROR: " & ex.Message)
+            MsgBox("an error occured. The record(s) could not be found")
+        Finally
+            oConnection.Close()
+
+        End Try
+
+        Return lsData
+    End Function
 #End Region
 
 #Region "HTML section"
@@ -801,5 +849,161 @@ Public Class ReportController
         Return sRows
     End Function
 #End Region
+
+#Region "2"
+
+    Public Sub createBreakReport(ByVal iMonths As Integer, ByVal iYears As Integer)
+        Debug.Print("CreatBreakReport...")
+
+        'lskey part 
+
+        Dim lsData = BreakReport1(iMonths, iYears)
+        Dim sReportTitle = "First controll break report "
+        Dim sReportContent = generateBreakReport(lsData, sReportTitle)
+        'lsData is ... sReporttiltle is 
+        Dim sReportFilename = "HRRIS First Control Break Report.html"
+        saveReport(sReportContent, sReportFilename)
+
+        Dim sParam As String = """" & Application.StartupPath & "\" & sReportFilename & """"
+        ' the """"" can fix into the access to the file path
+        Debug.Print("sParam: " & sParam)
+
+        System.Diagnostics.Process.Start(sParam)
+
+    End Sub
+
+    Private Function generateBreakReport(ByVal lsData As List(Of Hashtable), ByVal sReportTitle As String) As String
+        'This part seem like the code is going to take the value from 1.the lsDATA whihc was dimed before in the clas
+        'and the sReportitle whihc is going to be printed in the things ]
+
+        Debug.Print("GenerateBreakeReport ...")
+
+        Dim sReportContent As String
+
+        '1.Generate the start of the HTML file
+
+        Dim sDoctype As String = "<!DOCTYPE html>"
+        Dim sHtmlStartTag As String = "<html lang=""eng"">"
+        Dim sHeadTitle As String = "<head>" & vbCrLf &
+            "<title>" & sReportTitle & "</title>" & vbCrLf &
+        "<meta charset=""utf-8"">" & vbCrLf &
+        "<meta name=""viewport"" content=""width=device-width, initial-scale=1"">" & vbCrLf &
+        "<link rel=""stylesheet"" href=""https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"">" & vbCrLf &
+        "<script src=""https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js""></script>" & vbCrLf &
+        "<script src=""https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js""></script>" & vbCrLf &
+        "</head>"
+        Dim sBodyStartTag As String = "<body>"
+        Dim sReportHeading As String = "<h1>" & sReportTitle & "</h1>"
+        sReportContent = sDoctype & vbCrLf & sHtmlStartTag & vbCrLf & sHeadTitle & vbCrLf & sBodyStartTag & vbCrLf & sReportHeading & vbCrLf
+
+        '2.Generate the product table and its rows 
+        Dim sTable = generateControlBreakeTale(lsData)
+        sReportContent &= sTable & vbCrLf
+
+
+        '3.Generate the end of the HTML file 
+        Dim sBodyEndTag As String = "</body>"
+        Dim sHTMLEndTag As String = "</html>"
+        sReportContent &= sBodyEndTag & vbCrLf & sHTMLEndTag
+
+
+
+        Return sReportContent
+
+    End Function
+
+    Private Function generateControlBreakeTale(ByVal lsData As List(Of Hashtable)) As String
+        Dim sTable = "<table class =""table table-hover"">" & vbCrLf
+        Dim htSample As Hashtable = lsData.Item(0)
+        'Dim lsKeys = htSample.Keys
+        Dim lsKeys As List(Of String) = New List(Of String)
+        lsKeys.Add("booking_id")
+        lsKeys.Add("date")
+        lsKeys.Add("amount")
+
+
+        ' Generate the header row
+        Dim sHeadderRow = "<tr>" & vbCrLf
+        For Each key In lsKeys
+            sHeadderRow &= "<th>" & CStr(key) & "</th>" & vbCrLf
+        Next
+        sHeadderRow &= "</tr>" & vbCrLf
+        Debug.Print("sHeaderRow: " & sHeadderRow)
+        sTable &= sHeadderRow
+
+        'Generate the table rows 
+        sTable &= generateTableRows(lsData, lsKeys)
+
+        'Generate the end of the table 
+        sTable &= "</table>" & vbCrLf
+
+        Return sTable
+    End Function
+
+
+    Private Function generateTableRows(ByVal lsData As List(Of Hashtable), ByVal lsKeys As List(Of String)) As String
+
+        '1.Instalisation 
+        Dim sRows As String = ""
+        Dim sTableRow As String
+        Dim iCountRecordsPerCategory As Integer = 0
+        Dim bFirstTime As Boolean = True
+        Dim sCurrentControlField As String = ""
+        Dim sPreviousControlField As String = ""
+
+        '2. Loop through the list of hashtables
+        For Each record In lsData
+
+            '2a. Get a product and set the current key
+            Dim booking As Hashtable = record
+            sCurrentControlField = CStr(booking("booking_id"))
+
+            '2b. Do not check for control break on the first iteration of the loop
+            If bFirstTime Then
+                bFirstTime = False
+            Else
+                'Output total row if change in control field
+                'And reset the total
+                If sCurrentControlField <> sPreviousControlField Then
+                    sTableRow = "<tr><td colspan = """ & lsKeys.Count & """>" _
+                    & " Total invoice in " & sPreviousControlField _
+                        & " booking form: " & iCountRecordsPerCategory _
+                        & "</td></tr>" _
+                        & vbCrLf
+                    sRows &= sTableRow
+                    iCountRecordsPerCategory = 0
+                End If
+            End If
+
+            ' 2c. Output a normal row for every pass thru' the list
+'The part for each break record 
+            sTableRow = "<tr>" & vbCrLf
+            For Each key In lsKeys
+                sTableRow &= "<td>" & CStr(booking(key)) & "</td>" & vbCrLf
+'Cstr(invoice(date))
+            Next
+            sTableRow &= "</tr>" & vbCrLf
+            Debug.Print("sTableRow: " & sTableRow)
+            sRows &= sTableRow
+
+            '2d. Update control field and increment total
+            sPreviousControlField = sCurrentControlField
+            iCountRecordsPerCategory += 1
+        Next
+
+        '3. After the loop, need to output the last total row
+        sTableRow = "<tr><td colspan = """ & lsKeys.Count & """>" _
+                        & " Total Booking in " & iCountRecordsPerCategory _
+                        & " booking in the given year is : " & sCurrentControlField _
+                        & "</td></tr>" _
+                        & vbCrLf
+
+
+        sRows &= sTableRow
+
+        Return sRows
+    End Function
+
+#End Region 
 
 End Class
