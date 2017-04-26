2007e1d1de613bdf543ec5e455552ae39eeacb72
phamduyly <s3594800@rmit.edu.vn>
2017-04-26 14:59:45 +0700

    cmt invoice again, change to code

--- HRRIS/Invoice.vb
+++ HRRIS/Invoice.vb
@@ -2,4 +2,23 @@
     Private Sub invoice_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
-End Class
+End 
+
+
+# booking form section 
+'Fixing at form booking
+'Code to show bookinng form 
+
+'dim invoiceform as new Invoice
+'invoiceform.integerpass = txtbookingId.Text
+'invoiceform.show()
+'Me.hide
+
+# invoice form section 
+'fixing at form invoice 
+'before everything
+' public property IntegerPass as string
+
+'private sub frm_load
+' text box is id
+'id.text = IntegerPass
